d045ecf8c9395373f45f6552ac70d1ee6bc2c9e0
kartikarora <vbqca47lyam7dui5sqi1qq6c152vyr@bots.bitbucket.org>
2025-01-10 18:29:52 +0530

    commit code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -204,6 +204,7 @@ const Contact = () => {
 
           <div className="lg:w-full lg:h-72">
             <iframe
+              title="Google Maps- Onkar Digital Service"
               width="100%"
               height="100%"
               src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3080.8473589649675!2d78.06152887556436!3d30.319918774786032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzDCsDE5JzExLjciTiA3OMKwMDMnNTAuOCJF!5e1!3m2!1sen!2sin!4v1734350556142!5m2!1sen!2sin" 
